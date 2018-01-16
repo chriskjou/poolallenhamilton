96e396257d41b577c0176ae935975128f76583ad
elbertgong <elbertgong@college.harvard.edu>
2018-01-16 14:26:34 -0500

    small markov chain stuff

--- gamepreds/mcgamepred.py
+++ gamepreds/mcgamepred.py
@@ -6,5 +6,11 @@ trans_matrix = np.zeros((num_states, num_states))
 # markov chain: store transition probabilities
 
 def state_id(state):
+    idx = state[0]
+    for num in state[1:]:
+        idx *= 7
+        idx += num
+    return idx
 
-
+# change this
+data = get_data
