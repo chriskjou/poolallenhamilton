dd13bc1c111592abdfbdbd44318f453c30186570
elbertgong <elbertgong@college.harvard.edu>
2018-01-16 10:33:05 -0500

    why am i committing so many times

--- backsub/backsub2.py
+++ backsub/backsub2.py
@@ -2,13 +2,14 @@ import numpy as np
 import cv2
 
 
-original_image = cv2.imread('../cropped_images/01-27-1412frame4.jpg', cv2.IMREAD_COLOR)
+original_image = cv2.imread('../threshold.jpg', cv2.IMREAD_COLOR)
 gray_original = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-background_image = cv2.imread('../cropped_images/01-27-1412frame0.jpg', cv2.IMREAD_COLOR)
+background_image = cv2.imread('../threshold2.jpg', cv2.IMREAD_COLOR)
 gray_background = cv2.cvtColor(background_image, cv2.COLOR_BGR2GRAY)
 
 # foreground = np.absolute(gray_original - gray_background)
-foreground = cv2.absdiff(gray_original, gray_background)
+# foreground = cv2.absdiff(gray_original, gray_background)
+foreground = cv2.absdiff(original_image, background_image)
 # foreground[foreground > 0] = 255
 
 cv2.imshow('Original Image', foreground)
